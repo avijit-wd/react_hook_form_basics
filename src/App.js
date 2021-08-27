c101f3e33fabac9a1f84c2061af47382ff6b02ca
Avijit Biswas <avijitb440@gmail.com>
2021-08-27 16:47:50 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { TextField } from "@material-ui/core";
+
+import "./index.css";
 
 function App() {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="firstName"
+          control={control}
+          defaultValue=""
+          rules={{
+            required: {
+              value: true,
+              message: "This field is required",
+            },
+            minLength: {
+              value: 6,
+              message: "Password must be of 6 charecters",
+            },
+            validate: (value) => value === "123456",
+
+            // maxLength
+
+            // min
+
+            // max
+
+            // pattern
+          }}
+          render={({ field: { ref, ...field } }) => (
+            <TextField
+              {...field}
+              inputRef={ref}
+              id="firstName"
+              autoComplete="fname"
+              variant="outlined"
+              error={!!errors.firstName}
+              label="First Name"
+              helperText={
+                errors.firstName?.message ||
+                (errors.firstName?.type === "validate" &&
+                  "Password don't match")
+              }
+            />
+          )}
+        />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
